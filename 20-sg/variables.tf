933f3fd725f16740738897abf43aad30e622f872
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-27 19:38:32 -0700

    infra-dev

---
+++ 20-sg/variables.tf
@@ -0,0 +1,41 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+
+
+variable "common_tags" {
+    default = {
+        project = "expense"
+        environment = "dev"
+        terraform = true
+   }
+}
+
+variable "mysql_sg_tags" {
+    default = {
+        component = "mysql"
+    }
+}
+
+variable "backend_sg_tags" {
+    default = {
+        component = "backend"
+    }
+}
+
+variable "frontend_sg_tags" {
+    default = {
+        component = "frontend"
+    }
+} 
+
+variable "bastion_sg_tags" {
+    default = {
+        component = "bastion"
+    }
+}
